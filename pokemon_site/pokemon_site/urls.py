e64da015dffbf6b27e68ecb10861e5b7c3c6b709
santicr <santicr@javerianacali.edu.co>
2023-01-24 22:17:41 -0500

    All project: Removed some urls

--- pokemon_site/pokemon_site/urls.py
+++ pokemon_site/pokemon_site/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('api/1.0/', include(('api.urls', 'api'))),
-    path('api_generate_token/', views.obtain_auth_token),
 ]
